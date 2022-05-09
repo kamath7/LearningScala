1ff9971711a5d84bde3d00b8c53d926eecbc3446
kamath7 <adithyakamath96@gmail.com>
2022-05-09 18:57:05 +0530

    starting with functions

--- Expressions/src/main/scala/Expressions/Expressions.scala
+++ Expressions/src/main/scala/Expressions/Expressions.scala
@@ -11,4 +11,38 @@ object Expressions extends App{
   println(1 == 2)
 
   println(!(1 == x))
+
+  var myAwesomeVar: Int = 20
+  myAwesomeVar +=1
+  println(myAwesomeVar)
+
+  val someCond = true
+  val condVal = if(someCond) 69 else 50
+  print(condVal)
+
+
+  var myAwesomeLoopVar = 1
+
+  while(myAwesomeLoopVar < 10){
+    println(myAwesomeLoopVar)
+    myAwesomeLoopVar += 1
+  }
+
+  //dont write the above. part of imperative programming
+  //write expressions instead
+
+  val aWhile =  while(myAwesomeLoopVar < 10){
+    println(myAwesomeLoopVar)
+    myAwesomeLoopVar += 1
+  }
+
+  val someCodeBlock = {
+    val x = 2
+    val ss= 22
+    if (ss > 2) "Awesome" else "Buzz"
+  }
+
+  //println(ss) - > won't work. since scope is limited to someCodeBlock
+  println(someCodeBlock)
+  println(aWhile)
 }
---
+++ Functions/src/main/scala/Functions/Functions.scala
@@ -0,0 +1,10 @@
+package Functions
+
+object Functions extends App{
+
+  def myFunction(a: String, b: Int): String ={
+      return  "Hello, my name is "+a+" and my age is "+b
+  }
+  println(myFunction("Adithya",26))
+
+}
