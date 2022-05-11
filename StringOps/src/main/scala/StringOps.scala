099137bc9a31915e63e0f5449680d12a24d80da8
kamath7 <adithyakamath96@gmail.com>
2022-05-11 21:21:13 +0530

    some more string ops

--- StringOps/src/main/scala/StringOps.scala
+++ StringOps/src/main/scala/StringOps.scala
@@ -17,4 +17,20 @@ object StringOps extends App{
   println('a' +: numString  :+ 'z') //appending and prepending
   println(myStr.reverse)
   println(myStr.take(2))
+
+  //s-interpolator
+  val name = "Kams"
+  val age = 26
+  val greeting = s"Hello you are $name and you are $age years old"
+
+  println(greeting)
+  println(s"you were born in ${2022-age}")
+
+  //f - interpolator
+  val rate = 9.999f
+  val someStatement = f"$name can dance at $rate%2.2f steps per minute"
+  print(someStatement)
+
+  //raw interpolator
+  print(raw"Newline character \n ss") //ignores escape line
 }
