31f0cb9cbaca817d38292422f58da2ddbb7c9ad4
kamath7 <adithyakamath96@gmail.com>
2022-05-12 03:25:22 +0530

    auxillary constructors and overloading

--- ObjectOrientedParadigms/src/main/scala/MyOO.scala
+++ ObjectOrientedParadigms/src/main/scala/MyOO.scala
@@ -7,15 +7,22 @@ object MyOO extends App {
   //println(doctor.age) - invalid since not a field
   println(doctor.x)
   println(doctor.greets("Val"))
+  doctor.greets()
 }
 
-class Person(val name: String, age: Int){ //defining a constructor
+class Person(val name: String, age: Int = 0){ //defining a constructor . Default parameters also added
   //before instantion whole code block gets executed
   val x = 2
 
   println(x+1)
 
   def greets(name:String):Unit = println(s"${this.name} says hello to $name!")
+
+  //overloading method
+  def greets():Unit = println(s"${this.name} greets John Doe!")
+  //another constructor
+
+  def this(name:String) = this(name, 0) //same as in JAVA. DO NOT USE THIS!!
 }
 
 //class parameters cannot be accessed with . since they are not fields. to use add val to make them fieldss
