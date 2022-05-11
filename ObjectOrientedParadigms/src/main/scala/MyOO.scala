decdb96ed9b50c653e672c1d0cb1b3111a7365e3
kamath7 <adithyakamath96@gmail.com>
2022-05-12 03:19:33 +0530

    some more bits added to oop

--- ObjectOrientedParadigms/src/main/scala/MyOO.scala
+++ ObjectOrientedParadigms/src/main/scala/MyOO.scala
@@ -5,11 +5,17 @@ object MyOO extends App {
   println(doctor)
   println(doctor.name)
   //println(doctor.age) - invalid since not a field
-
+  println(doctor.x)
+  println(doctor.greets("Val"))
 }
 
 class Person(val name: String, age: Int){ //defining a constructor
+  //before instantion whole code block gets executed
+  val x = 2
 
+  println(x+1)
+
+  def greets(name:String):Unit = println(s"${this.name} says hello to $name!")
 }
 
 //class parameters cannot be accessed with . since they are not fields. to use add val to make them fieldss
