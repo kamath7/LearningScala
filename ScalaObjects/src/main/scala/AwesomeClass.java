43b9007e97aeb9e12271b15e0a21160ad331f0a3
kamath7 <adithyakamath96@gmail.com>
2022-05-13 01:30:24 +0530

    factory method, object

---
+++ ScalaObjects/src/main/scala/AwesomeClass.java
@@ -0,0 +1,9 @@
+public class AwesomeClass {
+    public static void main(String args[]){
+        System.out.println(Person.eyes);
+    }
+}
+class Person{
+    public static final int eyes = 2;
+
+}
---
+++ ScalaObjects/src/main/scala/MyAwesomeObj.scala
@@ -0,0 +1,29 @@
+object MyAwesomeObj extends App{
+  object Person{
+    //staticclass - level functionality
+    val N_EYES = 2
+    def canFly: Boolean = false
+    def apply(mother: Person, father: Person):Person = new Person("Bob") //this is a factory method
+  }
+
+  class Person(val name:String){
+    //instance level functionality
+  }
+
+  // can write object and Class person together. this is called companions
+  println(Person.N_EYES)
+  println(Person.canFly)
+
+  val harry = Person
+  val maguire = Person
+
+  val goat = new Person("Goat")
+  val messi = new Person("Messi")
+
+  println(goat == messi) //false
+  println(harry == maguire) //true
+  val child = Person(goat, messi)
+}
+//scala doesn't have class level functionality. No STATIC. Equivalent is Object
+// Scala object - singleton instance
+
