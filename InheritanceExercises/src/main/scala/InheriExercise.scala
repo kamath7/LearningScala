1f86208fd897a16414f972a05aaf82eeb3fed321
kamath7 <adithyakamath96@gmail.com>
2022-05-16 02:09:21 +0530

    generics added

--- InheritanceExercises/src/main/scala/InheriExercise.scala
+++ InheritanceExercises/src/main/scala/InheriExercise.scala
@@ -1,49 +1,50 @@
 object InheriExercise extends App {
 
-  abstract class MyList {
+  abstract class MyList[+A] {
 
-    def head:Int
-    def tail: MyList
+    def head:A
+    def tail: MyList[A]
     def isEmpty: Boolean
-    def add(element: Int):MyList
+    def add[ B >: A](element: B):MyList[B]
     def printElements:String
 
     override def toString: String = "{"+printElements+"}"
 
   }
 
-  object Empty extends MyList {
+  object Empty extends MyList[Nothing] {
     ///??? - means return nothing
-    override def head: Int = throw new NoSuchElementException()
+    override def head: Nothing = throw new NoSuchElementException()
 
-    override def tail: MyList = throw new NoSuchElementException()
+    override def tail: MyList[Nothing] = throw new NoSuchElementException()
 
     override def isEmpty: Boolean = true
 
-    override def add(element: Int): MyList = new Cons(element, Empty)
+    override def add[ B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
 
     def printElements:String = ""
   }
 
-  class Cons(h: Int, t:MyList) extends MyList{
-    override def head: Int = h
+  class Cons[+A](h: A, t:MyList[A]) extends MyList[A]{
+    override def head: A = h
 
-    override def tail: MyList = t
+    override def tail: MyList[A] = t
 
     override def isEmpty: Boolean = false
 
-    override def add(element: Int): MyList = new Cons(element, this)
+    override def add[B >: A](element: B): MyList[B] = new Cons(element, this)
 
     def printElements: String =
       if (t.isEmpty) ""+h
-      else h + " "+ t.printElements
+      else s"$h ${t.printElements}"
+
   }
 
 
 
-    val list = new Cons(1, new Cons(3, Empty))
-    print(list.tail.head)
-    print(list.add(5).head)
-    print(list.toString)
+//    val list = new Cons(1, new Cons(3, Empty))
+//    print(list.tail.head)
+//    print(list.add(5).head)
+//    print(list.toString)
 
 }
