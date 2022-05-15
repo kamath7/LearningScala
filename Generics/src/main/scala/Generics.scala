4c57eaabc8a5d93dd02ad9a08f342a778172889f
kamath7 <adithyakamath96@gmail.com>
2022-05-16 01:52:04 +0530

    different types of variant

--- Generics/src/main/scala/Generics.scala
+++ Generics/src/main/scala/Generics.scala
@@ -15,4 +15,26 @@ object Generics extends App {
   object lalleObj {
     def empty[A]: MyList[A] = ???
   }
+
+  val emptyListofInts = lalleObj.empty[Int]
+
+  //Variance problem
+
+  class Animal
+
+  class Cat extends Animal
+  class Bird extends Animal
+  //list of bird will extend list of Animal. This is called covariance.
+
+  class CovariantList[+A]
+  var animal:Animal = new Cat
+  val animalList:CovariantList[Animal] = new CovariantList[Cat]
+
+  //invariant list
+  class InvariantList[A]
+  val invariantAnimalList: InvariantList[Animal] = new InvariantList[Animal]
+
+  //contravariant list
+  class ContraVariantList[-A]
+  val contraVariantList: ContraVariantList[Cat] = new ContraVariantList[Animal]
 }
