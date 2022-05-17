9d0538b9b08fdc542b8431c5e4a8ab30b12b4418
kamath7 <adithyakamath96@gmail.com>
2022-05-18 02:10:17 +0530

    aliasing and imports

--- Packages/src/main/scala/SomePkg.scala
+++ Packages/src/main/scala/SomePkg.scala
@@ -1,7 +1,10 @@
 import Lalle.{Fiona, Lallex, hello}
 
 import java.io.Writer
-
+import java.sql
+import java.util.Date
+import java.sql.Date
+import java.util.{UUID => IDGen}
 object SomePkg extends App{
 
   val myLalle = new Lallex.Person("JJ",2000)
@@ -9,4 +12,10 @@ object SomePkg extends App{
 
   val awesome = new Fiona
   //packages ordered hierarchically. match folder structure
+
+  val myDate = new Date() //takes the new one
+  val sqlDate = new sql.Date() //takes the sql date but need to mention it fully
+  val myId = new IDGen(12,2) //aliasing
+
+  //default imports - java.lang
 }
