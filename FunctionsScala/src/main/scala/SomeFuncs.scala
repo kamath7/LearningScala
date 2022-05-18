b9f37f636bd2d467b74cb294885173b8f0549593
kamath7 <adithyakamath96@gmail.com>
2022-05-18 19:43:47 +0530

    some more funcs

--- FunctionsScala/src/main/scala/SomeFuncs.scala
+++ FunctionsScala/src/main/scala/SomeFuncs.scala
@@ -4,6 +4,15 @@ object SomeFuncs extends App{
     override def apply(element: Int): Int = element * 2
   }
   println(someFunc(2))
+
+  val StringToIntCov = new Function[String, Int]{
+    override def apply(v1: String): Int = v1.toInt
+  }
+  println(StringToIntCov("2"))
+
+  val adder: ((Int, Int) => Int)  = new Function2[Int, Int , Int] {
+    override def apply(a: Int, b:Int): Int = a+b
+  }
 }
 trait  MyFunc [A,B] {
   def apply(element: A):B
