64ab9c087a5eb6264c4c1d721d8c2f22fc08215d
kamath7 <adithyakamath96@gmail.com>
2022-05-18 19:46:56 +0530

    concatenator func

--- FunctionsScala/src/main/scala/SomeFuncs.scala
+++ FunctionsScala/src/main/scala/SomeFuncs.scala
@@ -13,6 +13,12 @@ object SomeFuncs extends App{
   val adder: ((Int, Int) => Int)  = new Function2[Int, Int , Int] {
     override def apply(a: Int, b:Int): Int = a+b
   }
+
+  def concatt: (String, String) => String = new Function2[String, String, String]{
+    override def apply(v1: String, v2: String): String = v1 + v2
+  }
+
+  println(concatt("Hello","Kams"))
 }
 trait  MyFunc [A,B] {
   def apply(element: A):B
