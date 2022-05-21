8a8b314dcff968063d75fefd4ba1678535f73c10
kamath7 <adithyakamath96@gmail.com>
2022-05-21 13:21:00 +0530

    arrays

---
+++ Collections-Sequences/src/main/scala/ArraysAwesome.scala
@@ -0,0 +1,10 @@
+object ArraysAwesome extends App{
+
+var nums = Array (1,2,3,4)
+val treeElems = Array.ofDim[Int](3)
+treeElems.foreach(println) //0 for primitves, null for others
+
+//mutations
+nums(2) = 88 //syntatic sugar for nums.update(2,88)
+println(nums.mkString(" "))
+}
