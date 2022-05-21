6fe690c111e49d7bac679947cf6d19e5edf4dcff
kamath7 <adithyakamath96@gmail.com>
2022-05-21 13:23:05 +0530

    implicit conversion

--- Collections-Sequences/src/main/scala/ArraysAwesome.scala
+++ Collections-Sequences/src/main/scala/ArraysAwesome.scala
@@ -7,4 +7,7 @@ treeElems.foreach(println) //0 for primitves, null for others
 //mutations
 nums(2) = 88 //syntatic sugar for nums.update(2,88)
 println(nums.mkString(" "))
+
+val numSeq: Seq[Int] = nums //implicit conversions
+println(numSeq)
 }
