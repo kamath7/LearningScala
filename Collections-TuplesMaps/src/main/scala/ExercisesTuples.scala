5c12a8f796878c6f80963c3bd988473f9dac4587
kamath7 <adithyakamath96@gmail.com>
2022-05-24 00:44:30 +0530

    Completed exercise pt1

--- Collections-TuplesMaps/src/main/scala/ExercisesTuples.scala
+++ Collections-TuplesMaps/src/main/scala/ExercisesTuples.scala
@@ -21,7 +21,7 @@ object ExercisesTuples extends App{
   def unfriend (network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
     val friendsA = network(a)
     val friendsB = network(b)
-    network + (a -> (friendsA - b) + (b -> (friendsB - a)))
+    network + (a -> (friendsA - b)) + (b -> (friendsB - a))
   }
 
   def remove(network: Map[String, Set[String]] ,person: String): Map[String, Set[String]] = {
@@ -33,4 +33,11 @@ object ExercisesTuples extends App{
     unfriended - person
   }
 
+  val empty: Map[String, Set[String]] = Map()
+  val network = add(add(empty, "Janes"), "Janus")
+  println(network)
+  println(friend(network, "Janes", "Janus"))
+
+ println(unfriend(friend(network, "Janes","Janus"), "Janes", "Janus"))
+ print(remove(friend(network, "Janes","Janus"), "Janes"))
 }
