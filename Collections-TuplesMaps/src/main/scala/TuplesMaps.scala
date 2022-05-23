6561c7a08caad8651487186079a7e2dfb9edfe65
kamath7 <adithyakamath96@gmail.com>
2022-05-23 13:16:52 +0530

    simple map

--- Collections-TuplesMaps/src/main/scala/TuplesMaps.scala
+++ Collections-TuplesMaps/src/main/scala/TuplesMaps.scala
@@ -10,5 +10,10 @@ object TuplesMaps extends App {
   println(myTUp.copy(_2 = "Lalle"))
   println(myTUp.swap)
 
+  //Maps - keys -> Values
+  val myMap: Map[String, Int] = Map()//string is key , int - value
+
+  val myContacts = Map(("Andy",888291), ("Robert California",2991002), "Michael Scott"->7812992) //michael scott one is similar but synctatic sugar
+  println(myContacts)
 
 }
