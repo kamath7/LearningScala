d9ec904a951c0fd84c9ee6edef1036530d93845b
kamath7 <adithyakamath96@gmail.com>
2022-05-24 01:27:29 +0530

    count of no friends and max friends

--- Collections-TuplesMaps/src/main/scala/ExercisesTuples.scala
+++ Collections-TuplesMaps/src/main/scala/ExercisesTuples.scala
@@ -1,34 +1,35 @@
 import TuplesMaps.myContacts
 
-object ExercisesTuples extends App{
+object ExercisesTuples extends App {
 
-  val someContacts = Map(("Jim" -> 1234), ("Jim"-> 9000))
+  val someContacts = Map(("Jim" -> 1234), ("Jim" -> 9000))
 
   println(someContacts.map(pair => pair._1.toLowerCase -> pair._2)) //use unique keys
 
 
-  def add(network: Map[String, Set[String]], person:String) : Map[String, Set[String]] = {
+  def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
     network + (person -> Set())
   }
 
-  def friend(network: Map[String, Set[String]], a: String, b:String ):Map[String, Set[String]] = {
+  def friend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
     val friendsA = network(a)
     val friendsB = network(b)
 
     network + (a -> (friendsA + b)) + (b -> (friendsB + a))
   }
 
-  def unfriend (network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+  def unfriend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
     val friendsA = network(a)
     val friendsB = network(b)
     network + (a -> (friendsA - b)) + (b -> (friendsB - a))
   }
 
-  def remove(network: Map[String, Set[String]] ,person: String): Map[String, Set[String]] = {
-    def removeAux (friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] ={
+  def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] = {
       if (friends.isEmpty) networkAcc
       else removeAux(friends.tail, unfriend(networkAcc, person, friends.head))
     }
+
     val unfriended = removeAux(network(person), network)
     unfriended - person
   }
@@ -38,19 +39,31 @@ object ExercisesTuples extends App{
   println(network)
   println(friend(network, "Janes", "Janus"))
 
- println(unfriend(friend(network, "Janes","Janus"), "Janes", "Janus"))
- print(remove(friend(network, "Janes","Janus"), "Janes"))
+  println(unfriend(friend(network, "Janes", "Janus"), "Janes", "Janus"))
+  print(remove(friend(network, "Janes", "Janus"), "Janes"))
 
- val people = add(add(add(empty, "Luke"), "Darth"), "Kylo")
- val lukeDarth = friend(people, "Luke","Darth")
- val test1 = friend(people, "Luke", "Kylo")
+  val people = add(add(add(empty, "Luke"), "Darth"), "Kylo")
+  val lukeDarth = friend(people, "Luke", "Darth")
+  val test1 = friend(people, "Luke", "Kylo")
 
- println(test1)
+  println(test1)
 
- def noOfFriends (network: Map[String, Set[String]], person:String): Int = {
-   if (!network.contains(person)) 0
-   else (network(person).size)
- }
+  def noOfFriends(network: Map[String, Set[String]], person: String): Int = {
+    if (!network.contains(person)) 0
+    else (network(person).size)
+  }
 
+  println(noOfFriends(test1, "Kylo"))
 
+  def maxFriends(network: Map[String, Set[String]]): String = {
+    network.maxBy(pair => pair._2.size)._1
+  }
+
+  println(maxFriends(network))
+
+  def noFriends(network: Map[String, Set[String]]): Int = {
+    network.view.filterKeys(k => network(k).isEmpty).size
+  }
+
+  println(noFriends(test1))
 }
