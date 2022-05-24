90d8ecb6638031f4050f6eddcdc76a0bdf4def1b
kamath7 <adithyakamath96@gmail.com>
2022-05-25 00:27:41 +0530

    connection class

--- TryCatch/src/main/scala/trycatch.scala
+++ TryCatch/src/main/scala/trycatch.scala
@@ -1,4 +1,4 @@
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Random, Success, Try}
 
 object trycatch extends  App {
 
@@ -32,6 +32,31 @@ object trycatch extends  App {
   println(success.map(_ * 2))
   println(success.flatMap(x => Success(x * 10)))
   println(success.filter(_ > 10))
+
+  //Exercise code
+
+  val hostName = "micasa"
+  val port = "8080"
+
+  def renderPage (page: String) = println(page)
+
+  class Connection {
+    def get (url: String): String = {
+      val random = new Random(System.nanoTime())
+      if (random.nextBoolean()) "Some HTML content"
+      else throw new RuntimeException("Connection gone")
+    }
+  }
+
+  object httpService {
+    val random = new Random(System.nanoTime())
+
+    def getConnection(host: String, port: String): Connection = {
+      if (random.nextBoolean()) new Connection
+      else throw new RuntimeException("PORT in use")
+    }
+  }
+
 
 
 }
