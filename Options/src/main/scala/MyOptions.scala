025e9bebbde9fd3bae3845362e36d95dce6a89cf
kamath7 <adithyakamath96@gmail.com>
2022-05-24 23:21:55 +0530

    exercise with connections

--- Options/src/main/scala/MyOptions.scala
+++ Options/src/main/scala/MyOptions.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 object MyOptions extends App {
 
   //option is a wrapper for a value that might be present or not
@@ -17,11 +19,13 @@ object MyOptions extends App {
 
   //chained mthods
 
-  def backup():String = "Some Result!"
+  def backup(): String = "Some Result!"
+
   val chainedRes = Option(unsafeOpts()).orElse(Option(backup())) //like promise chaining
 
   //Design Unsafe APIs
   def betterUnsafe(): Option[String] = None
+
   def betterBackup(): Option[String] = Some("Valid Result")
 
   val betterRes = betterUnsafe() orElse betterBackup()
@@ -35,4 +39,26 @@ object MyOptions extends App {
   println(myfirstOption.map(_ * 69))
   println(myfirstOption.filter(x => x > 10))
   println(myfirstOption.flatMap(x => Option(x * 10)))
+
+  //Exercise
+
+  val config: Map[String, String] = Map("host" -> "192.168.1.1", "Port" -> "8000")
+
+  class Connection {
+    def connect = "Connected!!"
+  }
+  object Connection{
+    var random = new Random(System.nanoTime())
+    def apply(host:String, port: String): Option[Connection] = {
+      if (random.nextBoolean()) Some(new Connection)
+      else None
+    }
+  }
+
+  val host = config.get("host")
+  val port = config.get("port")
+  val connection = host.flatMap(h => port.flatMap(p => Connection.apply(h, p)))
+  val connectionStatus = connection.map (c => c.connect)
+  println(connectionStatus)
+  connectionStatus.foreach(println)
 }
