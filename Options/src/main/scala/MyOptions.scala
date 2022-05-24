8967733c42814ef870cd132095a618b57d193bb6
kamath7 <adithyakamath96@gmail.com>
2022-05-24 23:09:33 +0530

    chaining methods

--- Options/src/main/scala/MyOptions.scala
+++ Options/src/main/scala/MyOptions.scala
@@ -15,4 +15,14 @@ object MyOptions extends App {
   val res = Option(unsafeOpts())
   print(res)
 
+  //chained mthods
+
+  def backup():String = "Some Result!"
+  val chainedRes = Option(unsafeOpts()).orElse(Option(backup())) //like promise chaining
+
+  //Design Unsafe APIs
+  def betterUnsafe(): Option[String] = None
+  def betterBackup(): Option[String] = Some("Valid Result")
+
+  val betterRes = betterUnsafe() orElse betterBackup()
 }
