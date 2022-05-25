bfca39b7ea138352522b098119ac38f1040dadde
kamath7 <adithyakamath96@gmail.com>
2022-05-25 19:39:37 +0530

    simple expression trait

--- PatternMatching/src/main/scala/PatternM.scala
+++ PatternMatching/src/main/scala/PatternM.scala
@@ -2,7 +2,7 @@ import scala.util.Random
 
 object PatternM extends App{
   val random = new Random
-  val x = random.nextInt()
+  val x = random.nextInt(5)
 
   val descriptor = x match {
     case 1 => "Numero UNO"
@@ -18,9 +18,35 @@ object PatternM extends App{
   val jim = Person("Jim", 23)
 
   val greeting = jim match {
-    case Person(n,a) => s"My name is $n and I'm $a years old"
-
+    case Person(n,a) if a < 21 => s"My name is $n and I'm $a years old and I'm not an adult"
+    case Person(n,a) if a > 21 => s"My name is $n and I'm $a years old and I'm an adult"
     case _ => "This is John Doe and I'm 69 years old"
   }
+
+  //cases are matched in order. no case match => match error
   println(greeting)
+
+  sealed class Animal
+  case class Dog(breed: String) extends Animal
+  case class Parrot (greeting: String) extends Animal
+
+  val animal: Animal = Dog("German Shepherd")
+
+  animal match {
+    case Dog(someBreed) => println(s"You are a dog of $someBreed! Woof Woof!")
+  }
+  //pattern matching works very well with case classes
+  //match-everything
+  val isEven = x match {
+    case n if n % 2 == 0 => true
+    case _ => false
+  }
+
+  //Exercise
+
+  trait expr
+  case class Number(n: Int) extends expr
+  case class Sum(e1: expr, e2: expr) extends expr
+  case class Prod(e1: expr, e2: expr) extends expr
+
 }
