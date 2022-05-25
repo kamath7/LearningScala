af2bf56f22099be7924da019874a20225df22716
kamath7 <adithyakamath96@gmail.com>
2022-05-26 01:42:11 +0530

    other kinds of pattern matching

--- PatternMatching/src/main/scala/AllPatterns.scala
+++ PatternMatching/src/main/scala/AllPatterns.scala
@@ -36,4 +36,39 @@ object AllPatterns extends App {
     case (_, (2, v)) =>
   }//patten matches can be nested
 
+  //List patterns
+
+  val myList = List(1,2,69,70)
+  val standardListMathc = myList match {
+    case List(1, _ , _ , _) => //extractor
+    case List(1, _*) => //list of arbitrary length
+    case 1 :: List(_) => //infix pattern
+    case List(1,2,3) :+ 42 => //infix pattern
+  }
+
+  //type specifiers
+
+  val unknown: Any = 2
+  val unknownMatch = unknown match {
+    case list: List[Int] => //explicit type specifier
+    case _ =>
+  }
+  //name binding
+//  val myList1 = List(1,2,69,70)
+//  val nameBindingMatch = myList1 match {
+//
+//    case nonEmptyList @ Cons(_, _) => //name binding
+//    case Cons(1, rest @ Cons(2, _)) => //name binding inside nesting patterns
+//  }
+
+  //multi pattern
+
+//  val multiPattern = myList match {
+//    case Empty | Cons(0, _) => //comppound Pattern
+//  }
+
+  //if guard
+//  val secondElement = myList match {
+//    case Cons(_, Cons(specialElement, _)) if specialElement % 2 == 0 =>
+//  }
 }
