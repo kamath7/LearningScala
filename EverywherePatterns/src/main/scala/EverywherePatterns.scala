e14cfd4b002b9a5bbb0c33fcc1bf538cbe4cfb7f
kamath7 <adithyakamath96@gmail.com>
2022-05-27 01:22:37 +0530

    partial functions

--- EverywherePatterns/src/main/scala/EverywherePatterns.scala
+++ EverywherePatterns/src/main/scala/EverywherePatterns.scala
@@ -14,12 +14,31 @@ object EverywherePatterns extends App {
   } yield 10 * x
   println(evenOnes)
 
-  val tuples = List((1,2), (4,5))
+  val tuples = List((1, 2), (4, 5))
   val filterTuples = for {
     (first, second) <- tuples
   } yield first + second
 
-  val tuple = (1,2,3)
-  val (a,b,c) = tuple
+  val tuple = (1, 2, 3)
+  val (a, b, c) = tuple
   println(s"$a, $b, $c")
+
+  val head :: tail = list
+  println(s" $head $tail ")
+
+  //partial func
+
+  val mapperList = list.map {
+    case v if v % 2 == 0 => v + "is even"
+    case 1 => "Number wan"
+    case _ => "Lalle"
+  } //partial func literal
+
+  val mapperList2 = list.map { x =>
+    x match {
+      case v if v % 2 == 0 => v + "is even"
+      case 1 => "Number wan"
+      case _ => "Lalle"
+    }
+  }
 }
